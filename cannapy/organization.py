9772661cb26776f167332b3fec8e8c7284e07f22
gregoryfoster <gregoryfoster@users.noreply.github.com>
2017-11-24 20:24:01 -0800

    Changed constructor to **kwargs; added licensed business class method

--- cannapy/organization.py
+++ cannapy/organization.py
@@ -2,10 +2,10 @@ class Organization(object):
     """An organization.
 
     Attributes:
-        name: A string representing the organization's name.
+        name: A string representing the organization's name
         ubi: The DOR-issued Unified Business Identifier
-        address_line_one: Organization physical address (first line)
-        address_line_two: Organization physical address (second line)
+        address_line_1: Organization physical address (first line)
+        address_line_2: Organization physical address (second line)
         city: Organization city
         state: Organization state
         zipcode: Organization zipcode
@@ -14,23 +14,45 @@ class Organization(object):
         license_id: The WSLCB-issued license identifier
         license_type: The license type
         license_status: The license status
-        license_creation_date: Date the license was issued
+        license_creation_date: The license creation/issue date
     """
 
-    def __init__(self, name, ubi, address_line_one, address_line_two, city,
-                 state, zipcode, county, phone, license_id, license_type,
-                 license_status, license_creation_date):
-        """Instantiate an Organization object."""
-        self.name = name
-        self.ubi = ubi
-        self.address_line_one = address_line_one
-        self.address_line_two = address_line_two
-        self.city = city
-        self.state = state
-        self.zipcode = zipcode
-        self.county = county
-        self.phone = phone
-        self.license_id = license_id
-        self.license_type = license_type
-        self.license_status = license_status
-        self.license_creation_date = license_creation_date
+    def __init__(self, **kwargs):
+        """Instantiate an Organization from given keyword arguments."""
+        default_values = {
+            'name': 'Organization Name',
+            'ubi': 'Unified Business Identifier',
+            'address_line_1': '',
+            'address_line_2': '',
+            'city': '',
+            'state': '',
+            'zipcode': '',
+            'county': '',
+            'phone': '',
+            'license_id': '',
+            'license_status': '',
+            'license_creation_date': ''
+        }
+
+        # Set instance properties from keyword arguments or default values
+        for (key, default) in default_values.items():
+            setattr(self, key, kwargs.get(key, default))
+
+    @classmethod
+    def from_licensed_business(cls, licensed_business):
+        """Instantiate an Organization from a WSLCB licensed business."""
+        organization = cls()
+        organization.name = licensed_business['organization']
+        organization.ubi = licensed_business['ubi']
+        organization.address_line_1 = licensed_business['address']
+        organization.address_line_2 = licensed_business['address_line_2']
+        organization.city = licensed_business['city']
+        organization.state = licensed_business['state']
+        organization.zipcode = licensed_business['zip']
+        organization.county = licensed_business['county']
+        organization.phone = licensed_business['dayphone']
+        organization.license_id = licensed_business['license']
+        organization.license_type = licensed_business['type']
+        organization.license_status = licensed_business['active']
+        organization.license_creation_date = licensed_business['createdate']
+        return organization
